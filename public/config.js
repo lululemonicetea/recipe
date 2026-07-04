52c326c6efdad8c6dd63f9562315bf85a9d33a33
lululemonicetea <lululemonicetea@gmail.com>
2026-07-05 02:39:47 +0900

    Add files via upload

--- public/config.js
+++ public/config.js
@@ -2,4 +2,4 @@
 // partners.coupang.com 대시보드 → 배너/위젯에서 만든 "iframe" 배너 코드를
 // 아래 따옴표 안에 붙여넣으세요. 비워두면 배너가 표시되지 않습니다.
 // 예: window.COUPANG_BANNER = '<iframe src="https://ads-partners.coupang.com/widgets.html?id=000000&..." width="300" height="250" frameborder="0"></iframe>';
-window.COUPANG_BANNER = "";
+window.COUPANG_BANNER = "<iframe src="https://ads-partners.coupang.com/widgets.html?id=1003193&template=carousel&trackingCode=AF3340136&subId=&width=300&height=300&tsource=" width="300" height="300" frameborder="0" scrolling="no" referrerpolicy="unsafe-url" browsingtopics></iframe>";
